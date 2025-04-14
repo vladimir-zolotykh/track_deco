9ff315ebd180df4ce1746c3ec70815c8dfa0103c
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-04-14 21:01:13 +0300

    Add @track decorator

--- deco.py
+++ deco.py
@@ -2,8 +2,26 @@
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
 import sys
+from functools import wraps
+import logging
 
 
+count: int = 0
+logging.basicConfig(level=logging.DEBUG)
+
+
+def track(func):
+    @wraps(func)
+    def wrapper(*args, **kw):
+        logging.info(f"About to call {func.__name__}")
+        res = func(*args, **kw)
+        logging.info(f"{func.__name__} took 3 seconds")
+        return res
+
+    return wrapper
+
+
+@track
 def factorial(n: int) -> int:
     if n < 2:
         return n
