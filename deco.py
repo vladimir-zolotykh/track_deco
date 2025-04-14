2c3dc76c928e7a5d0ddadf5cc9f02755b08913e8
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-04-14 21:29:23 +0300

    Add global variable `count'.
    Doesn't work for recursive calls

--- deco.py
+++ deco.py
@@ -11,20 +11,27 @@ count: int = 0
 logging.basicConfig(level=logging.DEBUG)
 
 
-def track(func):
-    @wraps(func)
-    def wrapper(*args, **kw):
-        logging.info(f"About to call {func.__name__}")
-        t1: float = time.perf_counter()
-        res = func(*args, **kw)
-        t2: float = time.perf_counter()
-        logging.info(f"{func.__name__} took {t2 - t1} seconds")
-        return res
+def track(limit: int = 3):
+    def decorate(func):
+        @wraps(func)
+        def wrapper(*args, **kw):
+            global count
+            if count < limit:
+                logging.info(f"About to call {func.__name__}")
+            count += 1
+            t1: float = time.perf_counter()
+            res = func(*args, **kw)
+            t2: float = time.perf_counter()
+            if count < limit:
+                logging.info(f"{func.__name__} took {t2 - t1} seconds")
+            return res
 
-    return wrapper
+        return wrapper
+
+    return decorate
 
 
-@track
+@track(3)
 def factorial(n: int) -> int:
     if n < 2:
         return n
